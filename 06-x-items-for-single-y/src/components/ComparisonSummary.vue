270dffb65542bdaeb83fc46b5827364b21f687e7
Erik Masny <erik.masny@gmail.com>
2022-12-11 20:18:34 +0100

    refactor(06): use computed value checking same items

--- 06-x-items-for-single-y/src/components/ComparisonSummary.vue
+++ 06-x-items-for-single-y/src/components/ComparisonSummary.vue
@@ -33,15 +33,19 @@ const moreExpensiveProduct = computed(() => {
 
   return props.productX
 })
+
+const itemsAreTheSame = computed(() => {
+  return props.productX?.id === props.productY?.id
+})
 </script>
 
 <template>
   <div v-if="productX !== null && productY !== null">
-    <p v-if="productX?.id !== productY?.id" class="mx-5 text-center">
-      You can get <strong>{{ timesMoreExpensive }}</strong> <em>{{ cheaperProduct?.title  }}</em> for about the same price as a single <em>{{ moreExpensiveProduct?.title }}</em>
-    </p>
-    <p v-else>
+    <p v-if="itemsAreTheSame">
       <em>These are the same items</em>
+    </p>
+    <p v-else class="mx-5 text-center">
+      You can get <strong>{{ timesMoreExpensive }}</strong> <em>{{ cheaperProduct?.title  }}</em> for about the same price as a single <em>{{ moreExpensiveProduct?.title }}</em>
     </p>
   </div>
 </template>
